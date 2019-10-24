1aca62b658a9800d7d44c39efa748c89bc9913ae
Wojciech Lukaszewicz <voytel@gmail.com>
2019-10-24 18:18:42 +1000

    Added UserName, PostAuthor, sortByAuthor, Final.

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 import { firebase } from './firebase/firebase'
 import LoadingPage from './components/LoadingPage'
-// import { startSetPostsNoAuth } from './actions/posts'
 
 const store = configureStore()
 const Jsx = () => (
@@ -21,29 +20,12 @@ const Jsx = () => (
 let hasRendered = false
 const renderApp = () => {
   if (!hasRendered) {
-    ReactDOM.render(<Jsx />, document.querySelector('#app'))
+    ReactDOM.render(<Jsx />, document.getElementById('app'))
     hasRendered = true
   }
 }
 
-ReactDOM.render(<LoadingPage />, document.querySelector('#app'))
-
-// firebase.auth().onAuthStateChanged((user) => {
-//   if (user) {
-//     store.dispatch(login(user.uid))
-//     store.dispatch(startSetPosts()).then(() => {
-//       renderApp()
-//       if (history.location.pathname === '/') {
-//         history.push('/dashboard')
-//       }
-//     })
-//   } else {
-//     store.dispatch(logout())
-//     renderApp()
-//     history.push('/')
-//   }
-// })
-
+ReactDOM.render(<LoadingPage />, document.getElementById('app'))
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
